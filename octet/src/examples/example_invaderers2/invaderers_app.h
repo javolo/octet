fe3888890f4aafe6e44ef6f6f3b8a889e52f63f4
Javier de la Osa <javo.osa@gmail.com>
2015-10-08 23:44:04 +0100

    Set 4 wall collision

--- octet/src/examples/example_invaderers2/invaderers_app.h
+++ octet/src/examples/example_invaderers2/invaderers_app.h
@@ -253,12 +253,17 @@ namespace octet {
 		  if (sprites[player_sprite].collides_with(sprites[first_border_sprite+3])) {
 			  sprites[player_sprite].translate(-player_speed, 0);
         }
-	  }  else if (is_key_down(key_up)) {
+	  } else if (is_key_down(key_up)) {
 		  // We calculate the things for the jump
-		  
 		  sprites[player_sprite].translate(0, +player_speed);
 		  if (sprites[player_sprite].collides_with(sprites[first_border_sprite+1])) {
 			  sprites[player_sprite].translate(0, -player_speed);
+		  }
+	  } else if (is_key_down(key_down)) {
+		  // We calculate the things for the jump
+		  sprites[player_sprite].translate(0, -player_speed);
+		  if (sprites[player_sprite].collides_with(sprites[first_border_sprite + 0])) {
+			  sprites[player_sprite].translate(0, +player_speed);
 		  }
 	  }
     }
