8ca39cfcceec2a90e5c9927f4d9cfcc76a951ada
javolo <javo.osa@gmail.com>
2016-10-23 12:11:47 +0100

    Shapes example in this project

--- octet/src/examples/toolmiddleware1/toolmiddleware1.h
+++ octet/src/examples/toolmiddleware1/toolmiddleware1.h
@@ -26,34 +26,45 @@ namespace octet {
     toolmiddleware1(int argc, char **argv) : app(argc, argv) {
     }
 
-    /// this is called once OpenGL is initialized
-    void app_init() {
-      app_scene =  new visual_scene();
-      app_scene->create_default_camera_and_lights();
+	/// this is called once OpenGL is initialized
+	void app_init() {
+		app_scene = new visual_scene();
+		app_scene->create_default_camera_and_lights();
+		app_scene->get_camera_instance(0)->get_node()->translate(vec3(0, 4, 0));
 
-      material *red = new material(vec4(1, 0, 0, 1));
-      mesh_box *box = new mesh_box(vec3(4));
-      scene_node *node = new scene_node();
-      app_scene->add_child(node);
-      app_scene->add_mesh_instance(new mesh_instance(node, box, red));
-    }
+		material *red = new material(vec4(1, 0, 0, 1));
+		material *green = new material(vec4(0, 1, 0, 1));
+		material *blue = new material(vec4(0, 0, 1, 1));
 
-    /// this is called to draw the world
-    void draw_world(int x, int y, int w, int h) {
-      int vx = 0, vy = 0;
-      get_viewport_size(vx, vy);
-      app_scene->begin_render(vx, vy);
+		mat4t mat;
+		mat.translate(-3, 6, 0);
+		app_scene->add_shape(mat, new mesh_sphere(vec3(2, 2, 2), 2), red, true);
 
-      // update matrices. assume 30 fps.
-      app_scene->update(1.0f/30);
+		mat.loadIdentity();
+		mat.translate(0, 10, 0);
+		app_scene->add_shape(mat, new mesh_box(vec3(2, 2, 2)), red, true);
 
-      // draw the scene
-      app_scene->render((float)vx / vy);
+		mat.loadIdentity();
+		mat.translate(3, 6, 0);
+		app_scene->add_shape(mat, new mesh_cylinder(zcylinder(vec3(0, 0, 0), 2, 4)), blue, true);
 
-      // tumble the box  (there is only one mesh instance)
-      scene_node *node = app_scene->get_mesh_instance(0)->get_node();
-      node->rotate(1, vec3(1, 0, 0));
-      node->rotate(1, vec3(0, 1, 0));
-    }
+		// ground
+		mat.loadIdentity();
+		mat.translate(0, -1, 0);
+		app_scene->add_shape(mat, new mesh_box(vec3(200, 1, 200)), green, false);
+	}
+
+	/// this is called to draw the world
+	void draw_world(int x, int y, int w, int h) {
+		int vx = 0, vy = 0;
+		get_viewport_size(vx, vy);
+		app_scene->begin_render(vx, vy);
+
+		// update matrices. assume 30 fps.
+		app_scene->update(1.0f / 30);
+
+		// draw the scene
+		app_scene->render((float)vx / vy);
+	}
   };
 }
