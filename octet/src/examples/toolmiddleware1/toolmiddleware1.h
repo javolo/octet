d477c835001bc369dfdfdb10064142615a85b205
javolo <javo.osa@gmail.com>
2016-10-23 12:05:27 +0100

    Comment at top

--- octet/src/examples/toolmiddleware1/toolmiddleware1.h
+++ octet/src/examples/toolmiddleware1/toolmiddleware1.h
@@ -1,9 +1,19 @@
 ////////////////////////////////////////////////////////////////////////////////
-//
-// (C) Andy Thomason 2012-2014
-//
-// Modular Framework for OpenGLES2 rendering on multiple platforms.
-//
+//																			  //
+// (C) Andy Thomason 2012-2014												  //
+//																			  //
+// Javier de la Osa Escalada												  //
+//																			  //
+// Modular Framework for OpenGLES2 rendering on multiple platforms.			  //
+//																			  //
+// Tools & Middleware Assignment 1											  //
+// Requirements:															  //
+// - Hinge constraints														  //
+// - Springe constraints													  //
+// - Collision callbacks													  //
+// - Read from CSV or text file												  //
+//																			  //
+////////////////////////////////////////////////////////////////////////////////
 
 
 namespace octet {
