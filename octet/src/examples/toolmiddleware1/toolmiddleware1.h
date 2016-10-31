089a68405c76d3af9331373603e8c79a9b9a656d
javolo <javo.osa@gmail.com>
2016-10-31 20:54:27 +0000

    Hinge and spring constraints working together

--- octet/src/examples/toolmiddleware1/toolmiddleware1.h
+++ octet/src/examples/toolmiddleware1/toolmiddleware1.h
@@ -52,52 +52,52 @@ namespace octet {
 
 		// Sphere 1
 		mat4t mat;
-		//mat.translate(-3, 0, 0);
-		//// Location Sphere 1
-		//btVector3 locationSphere1 = btVector3(-3, 0, 0);
-		//printf("Sphere 1 X(-3): %f \n", locationSphere1.getX());
-		//printf("Sphere 2 X(6): %f \n", locationSphere1.getY());
-		//printf("Sphere 3 X(0): %f \n", locationSphere1.getZ());
-		//// Add sphere to the scene
-		//app_scene->add_shape(mat, new mesh_sphere(vec3(2, 2, 2), 2), red, true, 10.0f);
+		mat.translate(-3, 0, 0);
+		// Location Sphere 1
+		btVector3 locationSphere1 = btVector3(-3, 0, 0);
+		printf("Sphere 1 X(-3): %f \n", locationSphere1.getX());
+		printf("Sphere 2 X(6): %f \n", locationSphere1.getY());
+		printf("Sphere 3 X(0): %f \n", locationSphere1.getZ());
+		// Add sphere to the scene
+		app_scene->add_shape(mat, new mesh_sphere(vec3(2, 2, 2), 2), red, true, 10.0f);
 
-		//// Sphere 2
-		//mat.loadIdentity();
-		//mat.translate(0, 6, 0);
-		//// Location Sphere 2
-		//btVector3 locationSphere2 = btVector3(0, 3, 0);
-		//app_scene->add_shape(mat, new mesh_sphere(vec3(2, 2, 2), 2), blue, true, 1.0f);
+		// Sphere 2
+		mat.loadIdentity();
+		mat.translate(0, 6, 0);
+		// Location Sphere 2
+		btVector3 locationSphere2 = btVector3(0, 3, 0);
+		app_scene->add_shape(mat, new mesh_sphere(vec3(2, 2, 2), 2), blue, true, 1.0f);
 
-		//// We create the HINGE Constraint
-		//// Definition taken from Bullet physics Wiki
-		//// http://bulletphysics.org/Bullet/BulletFull/classbtHingeConstraint.html#a5ae4261a17e0f0f8947025eae818f0c8
-		//// https://github.com/bulletphysics/bullet3/blob/master/examples/Constraints/TestHingeTorque.cpp
-		//// btHingeConstraint::btHingeConstraint(btRigidBody & rbA, btRigidBody & rbB, const btVector3 & pivotInA, const btVector3 & pivotInB, const btVector3 & axisInA, const btVector3 & axisInB, bool useReferenceFrameA = false )
-		//// We need:
-		//// 1. Rigid body Sphere 1 (btRigidBody, scene_node has one)
-		//// 2. Rigid body Sphere 2 (btRigidBody, scene_node has one)
-		//// 3. Location Sphere 1 (btVector3, with coordinates of rigid body)
-		//// 4. Location Sphere 2 (btVector3, with coordinates of rigid body)
-		//// 5. Axis (btVector3, axis X, Y and Z, depending where we want the hinge axis)
-		//
-		//// Rigid Body Sphere 1
-		//scene_node* sphere1 = app_scene->get_mesh_instance(0)->get_node();
-		//btRigidBody* rbSphere1 = sphere1->get_rigid_body();
-		//// We create a velocity vector to add some movements and see better the hinge constraint
-		//btVector3 velocitySphere1 = btVector3(0, 0, 0);
-		//rbSphere1->setLinearVelocity(velocitySphere1);
-		//// Rigid Body Sphere 2
-		//scene_node* sphere2 = app_scene->get_mesh_instance(1)->get_node();
-		//btRigidBody* rbSphere2 = sphere2->get_rigid_body();
-		//// Speed for sphere 2
-		//btVector3 velocitySphere2 = btVector3(100, 0, 100);
-		//rbSphere2->setLinearVelocity(velocitySphere2);
-		//// Axis (We´ll set the axis in the Y plane, but it can be changed easily)
-		//btVector3 axisY = btVector3(1, 0, 0);
-		//// With all this information we can create the bullet physics hinge constraint and see how that constraint work in the world
-		//btHingeConstraint* hinge = new btHingeConstraint(*rbSphere1, *rbSphere2, locationSphere1, locationSphere2, axisY, axisY);
-		//// Once we have created the constraint, we add it to the world
-		//gameWorld->addConstraint(hinge, true);
+		// We create the HINGE Constraint
+		// Definition taken from Bullet physics Wiki
+		// http://bulletphysics.org/Bullet/BulletFull/classbtHingeConstraint.html#a5ae4261a17e0f0f8947025eae818f0c8
+		// https://github.com/bulletphysics/bullet3/blob/master/examples/Constraints/TestHingeTorque.cpp
+		// btHingeConstraint::btHingeConstraint(btRigidBody & rbA, btRigidBody & rbB, const btVector3 & pivotInA, const btVector3 & pivotInB, const btVector3 & axisInA, const btVector3 & axisInB, bool useReferenceFrameA = false )
+		// We need:
+		// 1. Rigid body Sphere 1 (btRigidBody, scene_node has one)
+		// 2. Rigid body Sphere 2 (btRigidBody, scene_node has one)
+		// 3. Location Sphere 1 (btVector3, with coordinates of rigid body)
+		// 4. Location Sphere 2 (btVector3, with coordinates of rigid body)
+		// 5. Axis (btVector3, axis X, Y and Z, depending where we want the hinge axis)
+		
+		// Rigid Body Sphere 1
+		scene_node* sphere1 = app_scene->get_mesh_instance(0)->get_node();
+		btRigidBody* rbSphere1 = sphere1->get_rigid_body();
+		// We create a velocity vector to add some movements and see better the hinge constraint
+		btVector3 velocitySphere1 = btVector3(0, 0, 0);
+		rbSphere1->setLinearVelocity(velocitySphere1);
+		// Rigid Body Sphere 2
+		scene_node* sphere2 = app_scene->get_mesh_instance(1)->get_node();
+		btRigidBody* rbSphere2 = sphere2->get_rigid_body();
+		// Speed for sphere 2
+		btVector3 velocitySphere2 = btVector3(100, 0, 100);
+		rbSphere2->setLinearVelocity(velocitySphere2);
+		// Axis (We´ll set the axis in the Y plane, but it can be changed easily)
+		btVector3 axisY = btVector3(1, 0, 0);
+		// With all this information we can create the bullet physics hinge constraint and see how that constraint work in the world
+		btHingeConstraint* hinge = new btHingeConstraint(*rbSphere1, *rbSphere2, locationSphere1, locationSphere2, axisY, axisY);
+		// Once we have created the constraint, we add it to the world
+		gameWorld->addConstraint(hinge, true);
 
 
 		// We create the SPRING Constraint
@@ -112,11 +112,11 @@ namespace octet {
 
 		// Box 1
 		mat.loadIdentity();
-		mat.translate(0, 0, 0);
+		mat.translate(0, 0, -5);
 		app_scene->add_shape(mat, new mesh_box(vec3(2, 2, 2)), red, true);
 
 		// Rigid Body Box 1
-		scene_node* box1 = app_scene->get_mesh_instance(0)->get_node();
+		scene_node* box1 = app_scene->get_mesh_instance(2)->get_node();
 		btRigidBody* rbBox1 = box1->get_rigid_body();
 
 		// Transform Box 1
@@ -126,7 +126,7 @@ namespace octet {
 
 		// Box 2
 		mat.loadIdentity();
-		mat.translate(-3, 0, 0);
+		mat.translate(-3, 0, -5);
 		app_scene->add_shape(mat, new mesh_box(vec3(2, 2, 2)), blue, true);
 
 		// Transform Box 2
@@ -135,7 +135,7 @@ namespace octet {
 		frameInB.setOrigin(btVector3(btScalar(-7.0f), btScalar(0.0f), btScalar(0.0f)));
 
 		// Rigid Body Box 2
-		scene_node* box2 = app_scene->get_mesh_instance(1)->get_node();
+		scene_node* box2 = app_scene->get_mesh_instance(3)->get_node();
 		btRigidBody* rbBox2 = box2->get_rigid_body();
 
 		// Spring Constraint Definition with all the information created before
@@ -157,7 +157,7 @@ namespace octet {
 		gameWorld->addConstraint(springConstraint, true);
 		// More limits added
 		springConstraint->enableSpring(0, true);
-		springConstraint->setStiffness(0, 19.478f);
+		springConstraint->setStiffness(0, 39.478f);
 
 		// Cylinder
 		/*mat.loadIdentity();
