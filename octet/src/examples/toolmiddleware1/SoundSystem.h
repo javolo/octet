855a284e9808c86d753b1d1758d26e23d322c765
javolo <javo.osa@gmail.com>
2016-11-03 19:14:01 +0000

    create and play sound methods

--- octet/src/examples/toolmiddleware1/SoundSystem.h
+++ octet/src/examples/toolmiddleware1/SoundSystem.h
@@ -15,6 +15,7 @@ namespace octet {
 
 	public:
 
+		// Constructor of the class
 		SoundSystemClass() {
 			if (FMOD::System_Create(&m_pSystem) != FMOD_OK) {
 				// Report Error
@@ -31,6 +32,24 @@ namespace octet {
 
 			// Initialize our Instance with 36 Channels
 			m_pSystem->init(36, FMOD_INIT_NORMAL, NULL);
+		}
+
+		// Method that created the sound in the system
+		void createSound(SoundClass *pSound, const char* pFile) {
+			m_pSystem->createSound(pFile, FMOD_HARDWARE, 0, pSound);
+		}
+
+		// Method that play the sound when it´s called
+		void playSound(SoundClass pSound, bool bLoop = false) {
+			if (!bLoop)
+				pSound->setMode(FMOD_LOOP_OFF);
+			else
+			{
+				pSound->setMode(FMOD_LOOP_NORMAL);
+				pSound->setLoopCount(-1);
+			}
+
+			m_pSystem->playSound(FMOD_CHANNEL_FREE, pSound, false, 0);
 		}
 	}
 }
